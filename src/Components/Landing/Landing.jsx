4b4a42efdc304547c34fc206f2dd725eb2e1cd06
Bijay Jiwrajka <jiwrajkabijay@gmail.com>
2023-09-14 19:51:55 +0530

    Customise: Folder structure

---
+++ src/Components/Landing/Landing.jsx
@@ -0,0 +1,12 @@
+import style from "./Landing.module.scss";
+const Landing = () => {
+  return (
+    <div className={style.landing}>
+      <div className={style.heading}>
+        <p>Hello! This is our Landing Page</p>
+      </div>
+    </div>
+  );
+};
+
+export default Landing;
