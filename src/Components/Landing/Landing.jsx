1d2164826ae89549d97eedd93b830a4fa129c2ba
Bijay Jiwrajka <jiwrajkabijay@gmail.com>
2023-09-17 10:30:05 +0530

    CREATE: Hero section

--- src/Components/Landing/Landing.jsx
+++ src/Components/Landing/Landing.jsx
@@ -1,9 +1,25 @@
 import style from "./Landing.module.scss";
+import Lottie from "../../assets/Lotties/index";
 const Landing = () => {
   return (
     <div className={style.landing}>
-      <div className={style.heading}>
-        <p>Hello! This is our Landing Page</p>
+      <div className={style.landingContainer}>
+        <div className={style.leftlanding}>
+          <h1>Welcome to VedXpert</h1>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Excepturi
+            nemo sunt cum, voluptates facere aliquam sed illum! Pariatur
+            repellendus necessitatibus ipsa in fugiat beatae fugit corrupti!
+            Laborum suscipit incidunt odit?
+          </p>
+          <div className={style.landingbuttons}>
+            <button>Explore Now</button>
+            <button>Get Started</button>
+          </div>
+        </div>
+        <div className={style.rightlanding}>
+          <Lottie />
+        </div>
       </div>
     </div>
   );
